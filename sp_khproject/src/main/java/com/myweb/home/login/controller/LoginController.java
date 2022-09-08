aef46b07aeb799572a8582bc7739ca5051c8b154
예진욱 <isckd3@gmail.com>
2022-09-08 15:21:17 +0900

    회원가입 시 이메일 인증 추가 및 회원가입 로직 수정

--- sp_khproject/src/main/java/com/myweb/home/login/controller/LoginController.java
+++ sp_khproject/src/main/java/com/myweb/home/login/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.myweb.home.login.controller;
 
+import java.util.Properties;
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -7,11 +10,15 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,7 +30,7 @@ import com.myweb.home.login.service.LoginService;
 @RequestMapping(value="/login")
 public class LoginController {
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-		
+	Random rand = new Random();	
 	
 	@Autowired
 	private LoginService service;
@@ -57,9 +64,6 @@ public class LoginController {
 			}else {
 				return "login/m_login";
 			}
-			
-			
-
 	}
 	
 	@GetMapping(value="/sign")
@@ -76,7 +80,11 @@ public class LoginController {
 	
 	@PostMapping(value="/cussign")
 	public String cussign(HttpServletRequest request,
-						HttpSession session) {
+						HttpSession session
+						) {
+		
+		JSONObject json = new JSONObject();
+		
 		String cus_email = request.getParameter("cus_email");
 		String cus_name = request.getParameter("cus_name");
 		String cus_pw = request.getParameter("cus_pw");
@@ -101,25 +109,71 @@ public class LoginController {
 		data.setAc_index(10);
 		data.setAc_sendemail(cus_sendemail);
 		
-
-		if(cus_email == null) {
+		if(cus_email == null) {		// 이메일 입력 안했을 경우	
 			return "login/cussign";
 		}
+		
+		
+		if(cus_sendemail.equals("Y")) {		// 이메일 수신 동의를 했을 경우에만
 			
-	
-		boolean result = service.add(data);
-	
-		if(result) {
+			boolean result = service.add(data);		// DB 에 계정 데이터 추가
 			
-			data.setAc_email(cus_email);
-			data.setAc_pw(cus_pw);
-			
-			boolean result1 = service.getLogin(session, data);
-
-			return "redirect: /home/main";
+			if(result) {					// 계정 데이터가 추가되면
+				
+				data.setAc_email(cus_email);
+				data.setAc_pw(cus_pw);
+				
+				service.getLogin(session, data);
+				return "redirect: /home/main";
+			}
 		}
 		
 		return "login/cussign";
+	}
+	
+	// 이메일 인증
+	@PostMapping("/sendMail")
+	@ResponseBody
+	public String sendMail(@RequestParam(value="mail") String receiver_mail,
+			HttpServletRequest request,
+			Model model) {
+				
+		JSONObject json = new JSONObject();
+		
+		// 이메일 설정
+		JavaMailSender mailSender = null;
+		JavaMailSenderImpl senderImpl = new JavaMailSenderImpl();
+		senderImpl.setHost("smtp.gmail.com");		// gmail smtp 주소
+		senderImpl.setPort(587);					// smtp port 번호
+		senderImpl.setUsername("findofficial9@gmail.com");	// smtp 메일 계정
+		senderImpl.setPassword("enndjcfcetoipmsj");	// 앱 비밀번호
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);		// smtp 인증
+		prop.put("mail.smtp.starttls.enable", true);	// 보안 활성화
+		prop.put("mail.smtp.ssl.protocols", "TLSv1.3");	// protocol 종류
+		senderImpl.setJavaMailProperties(prop);
+		mailSender = senderImpl;
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		String[] to = {receiver_mail};	// 받는 사람 주소
+		message.setTo(to);
+		String[] cc = {};		// 참조 주소 (추가적으로 받는 사람 주소, 여기선 굳이 필요없음)
+		message.setCc(cc);
+		String[] bcc = {};		// 숨은 참조 주소 (수신자에게 참조인이 표시되지 않음, 여기선 굳이 필요없음)
+		message.setBcc(bcc);
+		
+		message.setSubject("[FIND 메일 인증]");	// 제목
+		
+		int randomNumber = rand.nextInt(888888) + 111111;
+		System.out.println("이메일 인증 번호 : " + randomNumber);
+		json.put("randomNumber", randomNumber);
+		message.setText(Integer.toString(randomNumber));	// 내용
+		mailSender.send(message);	// 전송
+		System.out.println(receiver_mail + "으로 이메일 인증번호 전송 완료");
+		
+		
+		return json.toJSONString();
+		
 	}
 	
 	@GetMapping(value="/selsign")
@@ -217,6 +271,13 @@ public class LoginController {
 			return json.toJSONString();
 			
 		}
+		
+
+		
+		
 	
-	
+		
+		
+		
+		
 }
--- sp_khproject/src/main/java/com/myweb/home/login/service/LoginService.java
+++ sp_khproject/src/main/java/com/myweb/home/login/service/LoginService.java
@@ -1,8 +1,14 @@
 package com.myweb.home.login.service;
 
+import java.util.Properties;
+import java.util.Random;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 import com.myweb.home.Accounts.model.AccountsDAO;
@@ -10,6 +16,7 @@ import com.myweb.home.Accounts.model.AccountsDTO;
 
 @Service
 public class LoginService {
+	Random rand = new Random();	
 	
 	@Autowired
 	private AccountsDAO dao;
@@ -80,6 +87,43 @@ public class LoginService {
 		}else {
 			return false;
 		}
+	}
+
+	public String joinEmail(String email) {
+		
+		int randomNumber = rand.nextInt(888888) + 111111;
+		System.out.println("인증번호 : " + randomNumber);
+		
+		// 이메일 설정
+		JavaMailSender mailSender = null;
+		JavaMailSenderImpl senderImpl = new JavaMailSenderImpl();
+		senderImpl.setHost("smtp.gmail.com");		// gmail smtp 주소
+		senderImpl.setPort(587);					// smtp port 번호
+		senderImpl.setUsername("findofficial9@gmail.com");	// smtp 메일 계정
+		senderImpl.setPassword("enndjcfcetoipmsj");	// 앱 비밀번호
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);		// smtp 인증
+		prop.put("mail.smtp.starttls.enable", true);	// 보안 활성화
+		prop.put("mail.smtp.ssl.protocols", "TLSv1.3");	// protocol 종류
+		senderImpl.setJavaMailProperties(prop);
+		mailSender = senderImpl;
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		String[] to = {email};	// 받는 사람 주소
+		message.setTo(to);
+		String[] cc = {};		// 참조 주소 (추가적으로 받는 사람 주소, 여기선 굳이 필요없음)
+		message.setCc(cc);
+		String[] bcc = {};		// 숨은 참조 주소 (수신자에게 참조인이 표시되지 않음, 여기선 굳이 필요없음)
+		message.setBcc(bcc);
+		
+		message.setSubject("[FIND 메일 인증]");	// 제목
+		
+		message.setText(Integer.toString(randomNumber));	// 내용
+		mailSender.send(message);	// 전송
+				
+				
+		
+		return Integer.toString(randomNumber);
 	}
 
 
