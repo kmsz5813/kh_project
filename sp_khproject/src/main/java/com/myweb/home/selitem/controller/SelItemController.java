8828695b24c38edc5bc7e5d266e61ccc5f58ebe0
김민성 <kmsz5813@naver.com>
2022-09-14 17:52:10 +0900

    .

--- sp_khproject/src/main/java/com/myweb/home/selitem/controller/SelItemController.java
+++ sp_khproject/src/main/java/com/myweb/home/selitem/controller/SelItemController.java
@@ -1,24 +1,12 @@
 package com.myweb.home.selitem.controller;
 
-import java.io.File;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.myweb.home.Accounts.model.AccountsDTO;
 import com.myweb.home.common.Paging;
@@ -28,10 +16,9 @@ import com.myweb.home.upload.model.FileUploadDTO;
 import com.myweb.home.upload.service.FileUploadService;
 
 @Controller
-@RequestMapping(value="/sellItem")
+@RequestMapping(value="/sellitem")
 public class SelItemController {
 
-	//getmapping을 해야지 페이지가 나온다고 생각하면돼 
 	@GetMapping(value="/additem")
 	public String additem(Model model) {
 		
@@ -48,43 +35,43 @@ public class SelItemController {
 		
 		//등록 로직 부분을 짜면 됨.
 		
-		return "redirect: /home/sellitem/additem";
+		return "sellitem/additem";
 	}
 	
 
-	
-	
-	@Autowired
-	private SelItemService service;
+//	
+//	
+//	@Autowired
+//	private SelItemService service;
 	
 //	 차후 상품등록 추가예정
 //	@Autowired
 //	private ProductUploadService
 	
-	@RequestMapping(value="", method=RequestMethod.GET)
-	public String getList(Model model, HttpSession session
-			, @RequestParam(defaultValue="1", required=false) int page
-			, @RequestParam(defaultValue="0", required=false) int pageCount) {
-		List datas = service.getAll();
-		
-		if(session.getAttribute("pageCount") == null) {
-			session.setAttribute("pageCount", 8);
-		}
-		
-		if(pageCount > 0) {
-			session.setAttribute("pageCount", pageCount);
-		}
-		else {
-			pageCount = Integer.parseInt(session.getAttribute("pageCount").toString());
-		}
-		
-		Paging paging = new Paging(datas, page, pageCount);
-		
-		model.addAttribute("datas", paging.getPageData());
-		model.addAttribute("pageData", paging);
-		
-		return "board/list";
-	}
+//	@RequestMapping(value="", method=RequestMethod.GET)
+//	public String getList(Model model, HttpSession session
+//			, @RequestParam(defaultValue="1", required=false) int page
+//			, @RequestParam(defaultValue="0", required=false) int pageCount) {
+//		List datas = service.getAll();
+//		
+//		if(session.getAttribute("pageCount") == null) {
+//			session.setAttribute("pageCount", 8);
+//		}
+//		
+//		if(pageCount > 0) {
+//			session.setAttribute("pageCount", pageCount);
+//		}
+//		else {
+//			pageCount = Integer.parseInt(session.getAttribute("pageCount").toString());
+//		}
+//		
+//		Paging paging = new Paging(datas, page, pageCount);
+//		
+//		model.addAttribute("datas", paging.getPageData());
+//		model.addAttribute("pageData", paging);
+//		
+//		return "board/list";
+//	}
 	
 //	@PostMapping(value="/add")
 //	public String add(HttpServletRequest request
--- sp_khproject/src/main/webapp/WEB-INF/views/sellitem/additem.jsp
+++ sp_khproject/src/main/webapp/WEB-INF/views/sellitem/additem.jsp
@@ -1,54 +1,69 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-	
-=======
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
-    
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+	<meta charset="UTF-8">
+	<title>상품 등록</title>
+	<%@ include file="../module/head.jsp" %>
 </head>
 <body>
+
+	<div class="jumbotron">
+		<div class="container">
+			<h1 class="display-10">
+				상품 등록
+			</h1>
+		</div>
+	</div>
+	
 	<header></header>
 	<section class="container w-75">
 	<div class="mt-5" >
 	<c:url var="addurl" value="/sellitem/additem" />
 	<form action="${addurl}" method="post">
-		<div>
-			상품등록
-		</div>
-		<div>
+		<div class="form-group row">
 		서비스
+			<form>
+       			<select name = "service">
+          		<option value = "It" selected>IT</option>
+          		<option value = "Sports">운동</option>
+          		<option value = "Lesson">음악</option>
+          		<option value = "Etc">기타</option>
+       			</select>
+    		</form>
 		</div>
-		<div>
+		<div class="form-group row">
 		지역
+			<form>
+       			<select name = "location">
+          		<option value = "Seoul" selected>서울</option>
+          		<option value = "Gyeonggi">경기</option>
+          		<option value = "Busan">부산</option>
+          		<option value = "Etc">기타</option>
+       			</select>
+    		</form>
 		</div>
-		<div>
+		<div class="form-group row">
 		내용추가 / 삭제
 		</div>
-		<div>
+		<div class="form-group row">
 		상품상세설명
-		<input type="text" class="form-control" name="test">
-		</div>
 		<div>
+			<textarea name="description" cols="50" placeholder="내용을 입력해주세요." rows="5" class="form-control"></textarea>
+		</div>
+		</div>
+		<div class="form-group row">
 		사진첨부
 		</div>
-		<button type="submit"  class="form-control"> 등록</button>
+		<div class="form-group row">
+		<button type="submit"  class="form-control">등록</button>
+		</div>
 	</form>
 	</div>
 	</section>
->>>>>>> branch '정종두' of https://github.com/kmsz5813/kh_project.git
 </body>
 </html>
