7c73b37cd113cc546ec6ec8f74561091d021fb35
이윤지 <dldbswl0820@naver.com>
2022-09-19 05:56:00 +0900

    커뮤궁금해요

--- sp_khproject/src/main/java/com/myweb/home/community/vo/CommunityQuestionVO.java
+++ sp_khproject/src/main/java/com/myweb/home/community/vo/CommunityQuestionVO.java
@@ -1,38 +1,34 @@
 package com.myweb.home.community.vo;
 
-public class CommunityVO {
-	private int cum_id;
-	private String cum_title;
-	private String cum_content;
-	
-	public int getCum_id() {
-		return cum_id;
+public class CommunityQuestionVO {
+	private int question_id;
+	private String question_title;
+	private String question_content;
+	public int getQuestion_id() {
+		return question_id;
 	}
-	
-	public void setCum_id(int cum_id) {
-		this.cum_id = cum_id;
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
 	}
-	
-	public String getCum_title() {
-		return cum_title;
+	public String getQuestion_title() {
+		return question_title;
 	}
-	
-	public void setCum_title(String cum_title) {
-		this.cum_title = cum_title;
+	public void setQuestion_title(String question_title) {
+		this.question_title = question_title;
 	}
-	
-	public String getCum_content() {
-		return cum_content;
+	public String getQuestion_content() {
+		return question_content;
 	}
-	
-	public void setCum_content(String cum_content) {
-		this.cum_content = cum_content;
+	public void setQuestion_content(String question_content) {
+		this.question_content = question_content;
 	}
-	
 	@Override
 	public String toString() {
-		return "CommunityVO [cum_id=" + cum_id + ", cum_title=" + cum_title + ", cum_content=" + cum_content + "]";
+		return "CommunityQuestionVO [question_id=" + question_id + ", question_title=" + question_title
+				+ ", question_content=" + question_content + "]";
 	}
+	
+	
 	
 	
 }
