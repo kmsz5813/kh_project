5a4d961ccb77947fb91184c6b3cab04945f35db0
채민선 <minsun2719@naver.com>
2022-09-26 17:37:59 +0900

    message

--- sp_khproject/src/main/java/com/myweb/home/message/service/MessageImpl.java
+++ sp_khproject/src/main/java/com/myweb/home/message/service/MessageImpl.java
@@ -14,16 +14,29 @@ public class MessageImpl implements MessageService {
 	@Autowired
 	private MessageDAO dao;
 	
-	// 채팅 대화 DB저장
-		@Override
-		public int insertChatting(ChattingCS ch) {
-			return dao.insertChatting(ch);
-		}
+//	// 채팅 대화 DB저장
+//		@Override
+//		public int insertChatting(chattingcs message) {
+//			return dao.insertChatting(message);
+//		}
+//
+//	// 채팅 내용 조회
+//		@Override
+//		public List<ChattingCS> selectChatting(ChattingCS ch) {
+//			return dao.selectChatting(ch);
+//		}
 
-	// 채팅 내용 조회
-		@Override
-		public List<ChattingCS> selectChatting(ChattingCS ch) {
-			return dao.selectChatting(ch);
-		}
+	@Override
+	public int insertChatting(ChattingCS ch) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<ChattingCS> selectChatting(ChattingCS ch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
